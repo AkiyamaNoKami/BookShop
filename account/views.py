08515a2b7c960ef9a7d4bde149b2ceec47d9a9ab
AmaneTsu <amanetsukasa724@gmail.com>
2023-10-08 19:10:29 +0300

    Update user registration

--- account/views.py
+++ account/views.py
@@ -1,16 +1,23 @@
+from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 from .forms import RegistrationForm
+from .models import UserBase
 from .token import account_activation_token
 
 
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/user/dashboard.html',)
+
 def account_register(request):
-    # if request.user.is_authenticated:
-    #     return redirect('/')
 
     if request.method == 'POST':
         registerForm = RegistrationForm(request.POST)
@@ -30,6 +37,21 @@ def account_register(request):
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
+            return HttpResponse('register successfully and activation sent')
     else:
         registerForm = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': registerForm})
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
