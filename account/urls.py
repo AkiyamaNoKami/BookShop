79fb8f45b8cf61a6103191be2ccc48d8696e027f
AmaneTsu <amanetsukasa724@gmail.com>
2023-10-09 16:24:25 +0300

    Login and Logout link updates

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from .forms import (UserLoginForm)
 from . import views
@@ -7,6 +7,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('account:login')), name='logout'),
     path('register/', views.account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     #User dashboard
