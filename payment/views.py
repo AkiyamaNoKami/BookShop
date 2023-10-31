8c1369686f7d20c93bc144912354548e86123127
AmaneTsu <amanetsukasa724@gmail.com>
2023-10-31 23:45:14 +0300

    Integrate Stripe system

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,21 @@
+import stripe
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from basket.basket import Basket
+
 @login_required
 def BasketView(request):
-    return render(request, 'payment/home.html')
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
+
+    stripe.api_key = 'pk_test_51O7Gu9HtpNtph5DTmlqA7aEGuejHJHmdM4QyGfFisW90r4sNcsnDMljM9y9K2xWU0jZtoiJFOqNvIAqbj67T7WlD00tj1lvd6s'
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency = 'gbp',
+        metadata = {'userid': request.user.id}
+    )
+
+    return render(request, 'payment/home.html', {'client_secret': intent.client_secret})
