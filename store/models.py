7e1b4b0f2266b0a1f1267386e25ca4fba44a5dcc
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-13 00:53:15 +0300

    Integrating db into templates

--- store/models.py
+++ store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from mptt.models import MPTTModel, TreeForeignKey
 from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel):
@@ -15,21 +15,9 @@ class Category(MPTTModel):
         max_length=255,
         unique=True,
     )
-    slug = models.SlugField(
-        verbose_name=_("Category safe URL"),
-        max_length=255,
-        unique=True
-    )
-    parent = TreeForeignKey(
-        "self",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="children"
-    )
-    is_active = models.BooleanField(
-        default=True
-    )
+    slug = models.SlugField(verbose_name=_("Category safe URL"), max_length=255, unique=True)
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    is_active = models.BooleanField(default=True)
 
     class MPTTMeta:
         order_insertion_by = ["name"]
@@ -47,18 +35,12 @@ class Category(MPTTModel):
 
 class ProductType(models.Model):
     """
-    ProductType Table will provide a list of the different typer of products that are for sale.
+    ProductType Table will provide a list of the different types
+    of products that are for sale.
     """
 
-    name = models.CharField(
-        verbose_name=_("Product Name"),
-        help_text=_("Required"),
-        max_length=255,
-        unique=True,
-    )
-    is_active = models.BooleanField(
-        default=True
-    )
+    name = models.CharField(verbose_name=_("Product Name"), help_text=_("Required"), max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _("Product Type")
@@ -70,18 +52,12 @@ class ProductType(models.Model):
 
 class ProductSpecification(models.Model):
     """
-    The Product Specification Table contains product specification or features for the product types.
+    The Product Specification Table contains product
+    specifiction or features for the product types.
     """
 
-    product_type = models.ForeignKey(
-        ProductType,
-        on_delete=models.RESTRICT
-    )
-    name = models.CharField(
-        verbose_name=_("Name"),
-        help_text=_("Required"),
-        max_length=255
-    )
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    name = models.CharField(verbose_name=_("Name"), help_text=_("Required"), max_length=255)
 
     class Meta:
         verbose_name = _("Product Specification")
@@ -96,33 +72,21 @@ class Product(models.Model):
     The Product table contining all product items.
     """
 
-    product_type = models.ForeignKey(
-        ProductType,
-        on_delete=models.RESTRICT
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.RESTRICT
-    )
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
     title = models.CharField(
         verbose_name=_("title"),
         help_text=_("Required"),
         max_length=255,
     )
-    description = models.TextField(
-        verbose_name=_("description"),
-        help_text=_("Not Required"),
-        blank=True
-    )
-    slug = models.SlugField(
-        max_length=255
-    )
+    description = models.TextField(verbose_name=_("description"), help_text=_("Not Required"), blank=True)
+    slug = models.SlugField(max_length=255)
     regular_price = models.DecimalField(
         verbose_name=_("Regular price"),
         help_text=_("Maximum 999.99"),
         error_messages={
             "name": {
-                "max_length": _("The price must be between 0 and 999.99"),
+                "max_length": _("The price must be between 0 and 999.99."),
             },
         },
         max_digits=5,
@@ -133,7 +97,7 @@ class Product(models.Model):
         help_text=_("Maximum 999.99"),
         error_messages={
             "name": {
-                "max_length": _("The price must be between 0 and 999.99"),
+                "max_length": _("The price must be between 0 and 999.99."),
             },
         },
         max_digits=5,
@@ -144,15 +108,8 @@ class Product(models.Model):
         help_text=_("Change product visibility"),
         default=True,
     )
-    created_at = models.DateTimeField(
-        _("Created at"),
-        auto_now_add=True,
-        editable=False,
-    )
-    updated_at = models.DateTimeField(
-        _("Updated at"),
-        auto_now=True,
-    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
 
     class Meta:
         ordering = ("-created_at",)
@@ -168,20 +125,15 @@ class Product(models.Model):
 
 class ProductSpecificationValue(models.Model):
     """
-    The Product Specification Value table holds each of the products individual specification or bespoke features.
+    The Product Specification Value table holds each of the
+    products individual specification or bespoke features.
     """
 
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE
-    )
-    specification = models.ForeignKey(
-        ProductSpecification,
-        on_delete=models.RESTRICT
-    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    specification = models.ForeignKey(ProductSpecification, on_delete=models.RESTRICT)
     value = models.CharField(
         verbose_name=_("value"),
-        help_text=_("Product specification value (maximum of 255 words)"),
+        help_text=_("Product specification value (maximum of 255 words"),
         max_length=255,
     )
 
@@ -198,11 +150,7 @@ class ProductImage(models.Model):
     The Product Image table.
     """
 
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name="product_image"
-    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
     image = models.ImageField(
         verbose_name=_("image"),
         help_text=_("Upload a product image"),
@@ -210,19 +158,15 @@ class ProductImage(models.Model):
         default="images/default.png",
     )
     alt_text = models.CharField(
-        verbose_name=_("Alternative text"),
-        help_text=_("Please add alternative text"),
+        verbose_name=_("Alturnative text"),
+        help_text=_("Please add alturnative text"),
         max_length=255,
         null=True,
         blank=True,
     )
-    is_feature = models.BooleanField(
-        default=False
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        editable=False
-    )
+    is_feature = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Product Image")
