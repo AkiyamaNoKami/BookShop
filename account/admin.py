53954d59fc5bfee066fa4fcd91b5c3de7bfdda5f
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-14 22:42:35 +0300

    Fixing problems with the changes we made in the account model

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import UserBase
+from .models import Customer
 
-class UserBaseAdmin(admin.ModelAdmin):
-    list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
 
-admin.site.register(UserBase, UserBaseAdmin)
+admin.site.register(Customer)
