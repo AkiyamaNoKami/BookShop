9bd7d3cb02a82522c13eb286cd8a5bb5152db706
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-15 21:26:49 +0300

    Add address

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,32 @@
 from django import forms
-from .models import Customer
+from .models import Customer, Address
 from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm, SetPasswordForm)
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ["full_name", "phone", "address_line", "address_line2", "town_city", "postcode"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["full_name"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["phone"].widget.attrs.update({"class": "form-control mb-2 account-form", "placeholder": "Phone"})
+        self.fields["address_line"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["address_line2"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["town_city"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["postcode"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(
@@ -13,9 +39,11 @@ class UserLoginForm(AuthenticationForm):
         }
     ))
 
+
 class RegistrationForm(forms.ModelForm):
     user_name = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
-    email = forms.EmailField(max_length=100, help_text='Required',error_messages={'required':'Sorry, you will need an email'})
+    email = forms.EmailField(max_length=100, help_text='Required',
+                             error_messages={'required': 'Sorry, you will need an email'})
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
@@ -45,20 +73,21 @@ class RegistrationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['user_name'].widget.attrs.update({'class': 'form-control mb-3', 'placeholder': 'Username'})
-        self.fields['email'].widget.attrs.update({'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_name'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_name'})
         self.fields['password'].widget.attrs.update({'class': 'form-control mb-3', 'placeholder': 'Password'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control mb-3', 'placeholder': 'Repeat Password'})
 
 
 class UserEditForm(forms.ModelForm):
-
     email = forms.EmailField(
         label='Account email (can not be changed)', max_length=200, widget=forms.TextInput(
             attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email', 'readonly': 'readonly'}))
 
     user_name = forms.CharField(
         label='Username', min_length=4, max_length=50, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-username', 'readonly':'readonly'}))
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-username',
+                   'readonly': 'readonly'}))
 
     first_name = forms.CharField(
         label='First Name', min_length=4, max_length=50, widget=forms.TextInput(
@@ -73,8 +102,8 @@ class UserEditForm(forms.ModelForm):
         self.fields['first_name'].required = True
         self.fields['email'].required = True
 
-class PwdResetForm(PasswordResetForm):
 
+class PwdResetForm(PasswordResetForm):
     email = forms.EmailField(max_length=254, widget=forms.TextInput(
         attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
 
@@ -85,6 +114,7 @@ class PwdResetForm(PasswordResetForm):
             raise forms.ValidationError(
                 'Unfortunatley we can not find that email address')
         return email
+
 
 class PwdResetConfirmForm(SetPasswordForm):
     new_password1 = forms.CharField(
--- account/views.py
+++ account/views.py
@@ -1,41 +1,40 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from django.shortcuts import render, redirect
-from .forms import RegistrationForm, UserEditForm
+from orders.views import user_orders
+
+from .forms import RegistrationForm, UserEditForm, UserAddressForm
 from .models import Customer, Address
 from .tokens import account_activation_token
-from orders.views import user_orders
 
 
 @login_required
 def dashboard(request):
     orders = user_orders(request)
     return render(request,
-                  'account/dashboard/dashboard.html', {'orders': orders})
+                  'account/dashboard/dashboard.html',
+                  {'section': 'profile', 'orders': orders})
 
 
 @login_required
 def edit_details(request):
-    update_successfully = False  # Изначально устанавливаем в False
-
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
 
         if user_form.is_valid():
             user_form.save()
-            update_successfully = True  # Если форма валидна, устанавливаем в True
-
     else:
         user_form = UserEditForm(instance=request.user)
 
-    return render(request, 'account/dashboard/edit_details.html',
-                  {'user_form': user_form, 'update_successfully': update_successfully})
+    return render(request,
+                  'account/dashboard/edit_details.html', {'user_form': user_form})
 
 
 @login_required
@@ -62,7 +61,7 @@ def account_register(request):
             current_site = get_current_site(request)
             subject = 'Activate your Account'
             message = render_to_string('account/registration/account_activation_email.html', {
-                'dashboard': user,
+                'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
@@ -89,9 +88,50 @@ def account_activate(request, uidb64, token):
         return render(request, 'account/registration/activation_invalid.html')
 
 
-#Addresses
+# Addresses
 
 @login_required
 def view_address(request):
     addresses = Address.objects.filter(customer=request.user)
     return render(request, "account/dashboard/addresses.html", {"addresses": addresses})
+
+
+@login_required
+def add_address(request):
+    if request.method == "POST":
+        address_form = UserAddressForm(data=request.POST)
+        if address_form.is_valid():
+            address_form = address_form.save(commit=False)
+            address_form.customer = request.user
+            address_form.save()
+            return HttpResponseRedirect(reverse("account:addresses"))
+    else:
+        address_form = UserAddressForm()
+    return render(request, "account/dashboard/edit_addresses.html", {"form": address_form})
+
+
+@login_required
+def edit_address(request, id):
+    if request.method == "POST":
+        address = Address.objects.get(pk=id, customer=request.user)
+        address_form = UserAddressForm(instance=address, data=request.POST)
+        if address_form.is_valid():
+            address_form.save()
+            return HttpResponseRedirect(reverse("account:addresses"))
+    else:
+        address = Address.objects.get(pk=id, customer=request.user)
+        address_form = UserAddressForm(instance=address)
+    return render(request, "account/dashboard/edit_addresses.html", {"form": address_form})
+
+
+@login_required
+def delete_address(request, id):
+    address = Address.objects.filter(pk=id, customer=request.user).delete()
+    return redirect("account:addresses")
+
+
+@login_required
+def set_default(request, id):
+    Address.objects.filter(customer=request.user, default=True).update(default=False)
+    Address.objects.filter(pk=id, customer=request.user).update(default=True)
+    return redirect("account:addresses")
